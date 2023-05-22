9f7a8480e9bb556135d2d88616f90cefd3fb26b0
francieO <58057814+francieO@users.noreply.github.com>
2023-05-22 14:39:22 -0400

    add bootstrap css for functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import HeaderNav from "./components/HeaderNav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HeaderNav />
+    </div>
+  );
 }
 
 export default App;
