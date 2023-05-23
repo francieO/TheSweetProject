77bcfec733a6730695bc6feaf23da0a2175eeba1
francieO <58057814+francieO@users.noreply.github.com>
2023-05-23 08:40:55 -0400

    update navbar links

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -7,23 +7,43 @@ function HeaderNav() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">The Sweet Project</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">HOME</Nav.Link>
+
+            <NavDropdown title="MENU" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">CAKES</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                DECORATED COOKIES
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="#action/3.3">MACARONS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">
+                BRIGADEIROS
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">CUPCAKES</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">CAKECICLES</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">CAKEPOPS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">
+                ARTESANAL COOKIE JARS
+              </NavDropdown.Item>
+              {/* <NavDropdown.Divider /> */}
+              {/* <NavDropdown.Item href="#action/3.4">
                 Separated link
+              </NavDropdown.Item> */}
+            </NavDropdown>
+            <NavDropdown title="GALLERY" id="basic-nav-dropdown">
+              {" "}
+              <NavDropdown.Item href="#action/3.1">
+                SWEET GALLERY
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">
+                CAKE GALLERY
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#link">ABOUT</Nav.Link>
+            <Nav.Link href="#link">CONTACT</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
